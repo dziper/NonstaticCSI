a264a32db33e901292c062e33a689f3c377be352
ibraahiim <ibrahim35kilinc@gmail.com>
2024-12-03 22:22:04 -0800

    data gen

--- experimentation/dict_stuff.py
+++ experimentation/dict_stuff.py
@@ -1,27 +1,75 @@
 import os
 import pickle
 import numpy as np
-def load_pickle_files(directory):
+def combine_time_series_paths(directory,path_indices,desired_freq,save_filename="train.pickle"):
     list_of_dicts = []
     if not os.path.isdir(directory):
         raise ValueError(f"The specified directory does not exist: {directory}")
+
+    channels = []
+    locations = []
+    speeds = []
+    ant_orients = []
+
+    is_reverse = False
     for filename in os.listdir(directory):
         # Check if the file is a pickle file
-        if filename.endswith('.pkl') or filename.endswith('.pickle'):
+        if "fc" in filename:
             # Construct full file path
             filepath = os.path.join(directory, filename)
-            with open(filepath, 'rb') as f:
-                # Store the contents in the dictionary, using filename as the key
-                data = pickle.load(f)
+
+            path_ind = int(filename.split("_")[0][0:-4])
+            freq = int(filename.split("_")[6].split(".")[0])
+
+            if(path_ind in path_indices and freq == desired_freq):
+
+                with open(filepath, 'rb') as f:
+                    # Store the contents in the dictionary, using filename as the key
+                    data = pickle.load(f)
+
                 freq_channel = np.array(data["freq_channel"])
-                freq_channel = np.squeeze(freq_channel)
+                # freq_channel = np.squeeze(freq_channel)
                 ue_loc = np.array(data["UE_loc"])
                 ue_speed = np.array(data["UE_speed"])
                 antenna_orient = np.array(data["antenna_orient"])
-                pickle_data = {"CSI": freq_channel, "Location": ue_loc, "Speed": ue_speed, "Antenna Orientation": antenna_orient}
-                list_of_dicts.append(pickle_data)
-    return list_of_dicts
 
-directory_path = r"C:\Users\nrazavi\Downloads\drive-download-20241204T004446Z-001"
+                if(is_reverse):
+                    channels.append(freq_channel[::-1])
+                    locations.append(ue_loc[::-1])
+                    speeds.append(ue_speed[::-1])
+                    ant_orients.append(antenna_orient[::-1])
+                    is_reverse = False
+                else:
+                    channels.append(freq_channel)
+                    locations.append(ue_loc)
+                    speeds.append(ue_speed)
+                    ant_orients.append(antenna_orient)
+                    is_reverse = True
 
-loaded_pickles = load_pickle_files(directory_path)
+    channels = np.vstack(channels)
+    speeds = np.vstack(speeds)
+    ant_orients = np.vstack(ant_orients)
+    locations = np.vstack(locations)
+
+    data = {}
+    data["freq_channel"] = channels
+    data["UE_loc"] = locations
+    data["UE_speed"] = speeds
+    data["antenna_orient"] = ant_orients
+
+    print("Saving files to ",os.path.join(directory, save_filename))
+    print("Number of samples",data["freq_channel"].shape)
+    with open(os.path.join(directory, save_filename), "wb") as f:
+        pickle.dump(data, f)
+
+
+directory = f"C:\\Users\ibrahimkilinc\Documents\ECE257_Project\\NonstaticCSI\Dataset"
+desired_fc = 2.5e9
+train_dir = f"train_series.pickle"
+train_indices = [i for i in range(33)]
+
+test_indices = [i for i in range(34,40)]
+combine_time_series_paths(directory,train_indices, desired_fc, train_dir)
+
+test_dir = f"test_series.pickle"
+combine_time_series_paths(directory,test_indices, desired_fc, test_dir)
